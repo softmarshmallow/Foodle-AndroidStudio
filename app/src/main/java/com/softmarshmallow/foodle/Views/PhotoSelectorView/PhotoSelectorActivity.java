caaa545b5d299d5c5f92c636a9568b68f8bd4d05
yuntaeil <zkrxpdlf0808@gmail.com>
2018-01-09 17:19:26 +0900

    Create PhotoSelecter files dummy

---
+++ app/src/main/java/com/softmarshmallow/foodle/Views/PhotoSelectorView/PhotoSelectorActivity.java
@@ -0,0 +1,15 @@
+package com.softmarshmallow.foodle.Views.PhotoSelectorView;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.softmarshmallow.foodle.R;
+
+public class PhotoSelectorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo_selector_view);
+    }
+}
