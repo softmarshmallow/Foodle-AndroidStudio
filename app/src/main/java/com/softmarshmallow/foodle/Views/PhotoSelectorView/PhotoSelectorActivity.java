7720e5246badb988ad4002a55f3fa2288fa64146
yuntaeil <zkrxpdlf0808@gmail.com>
2018-01-13 22:43:33 +0900

    Error

--- app/src/main/java/com/softmarshmallow/foodle/Views/PhotoSelectorView/PhotoSelectorActivity.java
+++ app/src/main/java/com/softmarshmallow/foodle/Views/PhotoSelectorView/PhotoSelectorActivity.java
@@ -1,15 +1,152 @@
 package com.softmarshmallow.foodle.Views.PhotoSelectorView;
 
-import android.support.v7.app.AppCompatActivity;
+import android.animation.ObjectAnimator;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.util.Pair;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.view.View;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.softmarshmallow.foodle.R;
+import com.woxthebox.draglistview.BoardView;
+import com.woxthebox.draglistview.DragItem;
+
+import java.util.ArrayList;
 
 public class PhotoSelectorActivity extends AppCompatActivity {
 
+    private static int sCreatedItems = 0;
+    private BoardView mBoardView;
+    private int mColumns;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_photo_selector_view);
+        mBoardView = (BoardView) findViewById(R.id.board_view);
+        mBoardView.setSnapToColumnsWhenScrolling(true);
+        mBoardView.setSnapToColumnWhenDragging(true);
+        mBoardView.setSnapDragItemToTouch(true);
+        mBoardView.setCustomDragItem(new MyDragItem(this, R.layout.card_photoselecter));
+        mBoardView.setSnapToColumnInLandscape(false);
+        mBoardView.setColumnSnapPosition(BoardView.ColumnSnapPosition.CENTER);
+
+        mBoardView.setBoardListener(new BoardView.BoardListener() {
+            @Override
+            public void onItemDragStarted(int column, int row) {
+                Toast.makeText(mBoardView.getContext(), "Start - column: " + column + " row: " + row, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onItemChangedPosition(int oldColumn, int oldRow, int newColumn, int newRow) {
+                Toast.makeText(mBoardView.getContext(), "Position changed - column: " + newColumn + " row: " + newRow, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onItemChangedColumn(int i, int i1) {
+
+            }
+
+            @Override
+            public void onItemDragEnded(int fromColumn, int fromRow, int toColumn, int toRow) {
+                if (fromColumn != toColumn || fromRow != toRow) {
+                    Toast.makeText(mBoardView.getContext(), "End - column: " + toColumn + " row: " + toRow, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        mBoardView.setBoardCallback(new BoardView.BoardCallback() {
+            @Override
+            public boolean canDragItemAtPosition(int column, int dragPosition) {
+                // Add logic here to prevent an item to be dragged
+                return true;
+            }
+
+            @Override
+            public boolean canDropItemAtPosition(int oldColumn, int oldRow, int newColumn, int newRow) {
+                // Add logic here to prevent an item to be dropped
+                return true;
+            }
+        });
+        ((AppCompatActivity) this).getSupportActionBar().setTitle("Board");
+
+        addColumnList();
+        addColumnList();
+        addColumnList();
+        addColumnList();
+        addColumnList();
+    }
+
+
+    private void addColumnList() {
+        final ArrayList<Pair<Long, String>> mItemArray = new ArrayList<>();
+        int addItems = 15;
+        for (int i = 0; i < addItems; i++) {
+            long id = sCreatedItems++;
+            mItemArray.add(new Pair<>(id, "Item " + id));
+        }
+
+        final int column = mColumns;
+        final PhotoSelectorItemAdapter listAdapter = new PhotoSelectorItemAdapter(mItemArray, R.layout.card_photoselecter , R.id.card_photoselecter, true);
+
+        mColumns++;
+    }
+
+    private static class MyDragItem extends DragItem {
+
+        MyDragItem(Context context, int layoutId) {
+            super(context, layoutId);
+        }
+
+        @Override
+        public void onBindDragView(View clickedView, View dragView) {
+            CharSequence text = ((TextView) clickedView.findViewById(R.id.text)).getText();
+            ((TextView) dragView.findViewById(R.id.text)).setText(text);
+            CardView dragCard = ((CardView) dragView.findViewById(R.id.card_photoselecter));
+            CardView clickedCard = ((CardView) clickedView.findViewById(R.id.card_photoselecter));
+
+            dragCard.setMaxCardElevation(40);
+            dragCard.setCardElevation(clickedCard.getCardElevation());
+            // I know the dragView is a FrameLayout and that is why I can use setForeground below api level 23
+            //  dragCard.setForeground(clickedView.getResources().getDrawable(R.drawable.card_view_drag_foreground));
+        }
+
+        @Override
+        public void onMeasureDragView(View clickedView, View dragView) {
+            CardView dragCard = ((CardView) dragView.findViewById(R.id.card_photoselecter));
+            CardView clickedCard = ((CardView) clickedView.findViewById(R.id.card_photoselecter));
+            int widthDiff = dragCard.getPaddingLeft() - clickedCard.getPaddingLeft() + dragCard.getPaddingRight() -
+                    clickedCard.getPaddingRight();
+            int heightDiff = dragCard.getPaddingTop() - clickedCard.getPaddingTop() + dragCard.getPaddingBottom() -
+                    clickedCard.getPaddingBottom();
+            int width = clickedView.getMeasuredWidth() + widthDiff;
+            int height = clickedView.getMeasuredHeight() + heightDiff;
+            dragView.setLayoutParams(new FrameLayout.LayoutParams(width, height));
+
+            int widthSpec = View.MeasureSpec.makeMeasureSpec(width, View.MeasureSpec.EXACTLY);
+            int heightSpec = View.MeasureSpec.makeMeasureSpec(height, View.MeasureSpec.EXACTLY);
+            dragView.measure(widthSpec, heightSpec);
+        }
+
+        @Override
+        public void onStartDragAnimation(View dragView) {
+            CardView dragCard = ((CardView) dragView.findViewById(R.id.card_photoselecter));
+            ObjectAnimator anim = ObjectAnimator.ofFloat(dragCard, "CardElevation", dragCard.getCardElevation(), 40);
+            anim.setInterpolator(new DecelerateInterpolator());
+            anim.setDuration(ANIMATION_DURATION);
+            anim.start();
+        }
+
+        @Override
+        public void onEndDragAnimation(View dragView) {
+            CardView dragCard = ((CardView) dragView.findViewById(R.id.card_photoselecter));
+            ObjectAnimator anim = ObjectAnimator.ofFloat(dragCard, "CardElevation", dragCard.getCardElevation(), 6);
+            anim.setInterpolator(new DecelerateInterpolator());
+            anim.setDuration(ANIMATION_DURATION);
+            anim.start();
+        }
     }
 }
